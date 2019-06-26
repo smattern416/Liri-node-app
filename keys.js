f378e2a1d6293e67616f92aa55d1e842fe7ca364
Stephanie Mattern <smattern416@gmail.com>
2019-06-26 08:23:00 -0400

    added function for omdb to liri.js

--- keys.js
+++ keys.js
@@ -5,3 +5,12 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 };
 
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = {
+  api_key: process.env.OMDB_API_KEY
+};
+
--- liri.js
+++ liri.js
@@ -9,11 +9,13 @@ var Spotify = require('node-spotify-api');
 var whatToDo = process.argv[2];
 var userInput = process.argv[3];
 
+// add for loop so it allows more than one word items
+
 function spotifyThis(){
     spotify
-  .search({ type: 'track', query: 'All the Small Things', limit: 5 })
+  .search({ type: 'track', query: userInput, limit: 5 })
   .then(function(response) {
-    console.log(response);
+    console.log(JSON.stringify(response.tracks.items, null, 2));
   })
   .catch(function(err) {
     console.log(err);
@@ -36,3 +38,61 @@ switch (whatToDo) {
         break;
 
 }
+
+
+function spotifyThisSong(song){
+  spotify.search({ type: 'track', query: song, limit: 1}, function(error, data){
+      if(!error){
+      for(var i = 0; i < data.tracks.items.length; i++){
+          var songData = data.tracks.items[i];
+                    //artist
+          console.log("Artist: " + songData.artists[0].name);
+                    //song name
+          console.log("Song: " + songData.name);
+                    //spotify preview link
+          console.log("Preview URL: " + songData.preview_url);
+                    //album name
+          console.log("Album: " + songData.album.name);
+          console.log("-----------------------");
+          } 
+      } else {
+      console.log('Error occurred.');
+      }
+  });
+  }
+
+  function omdb(movie){
+      var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&apikey=' + omdbKey + '&plot=short&tomatoes=true';
+    
+      request(omdbURL, function (error, response, body){
+        if(!error && response.statusCode == 200){
+          var body = JSON.parse(body);
+    
+          console.log("Title: " + body.Title);
+          console.log("Release Year: " + body.Year);
+          console.log("IMdB Rating: " + body.imdbRating);
+          console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+          console.log("Country: " + body.Country);
+          console.log("Language: " + body.Language);
+          console.log("Plot: " + body.Plot);
+          console.log("Actors: " + body.Actors);
+          
+          
+        } else{
+          console.log('Error occurred.')
+        }
+          if(movie === "Mr. Nobody"){
+            console.log("-----------------------");
+            console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("It's on Netflix!");
+        }
+      });
+    
+    }
+    function doThing(){
+      fs.readFile('random.txt', "utf8", function(error, data){
+        var txt = data.split(',');
+    
+        spotifyThisSong(txt[1]);
+      });
+    }
